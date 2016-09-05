3f100ea3285aa31718b89682aea9353729dd3ee1
fcipriano <etefcipriano@gmail.com>
2016-09-05 21:58:43 +0800

    updated file_action.php

--- phpapp141/file_action.php
+++ phpapp141/file_action.php
@@ -1,24 +1,35 @@
 <?php 
+
+include('/calss/database.php');
+
+$odb = new Database();
+$userid = $_SESSION["username"];
+
 $TARGET_DIR = "uploads/";
 $file = $_FILES["profilepic"];
 $filename = $file["name"];
 $newfilename = date("Y-m-d-h-i-sa");
 
 $fileext = pathinfo($filename,PATHINFO_EXTENSION);//gets file extension
+$newfilename .= $fileext;
 
-$target_file = $TARGET_DIR . $newfilename. "." . $fileext;//creates a new file name and target path
+$target_file = $TARGET_DIR . $newfilename;//creates a new file name and target path
 
 $filesize= $file["size"];
 
 echo $filesize;
 
-/*
+
 //uploads the file to target path
 if (move_uploaded_file($_FILES["profilepic"]["tmp_name"], $target_file)) {
 
-//save filename to database
-	echo "The file has been uploaded.";
+    //save filename to database
+
+    $sqlstr = "UPDATE users SET profilepic='"+$newfilename +"' WHERE userid='"+ $userid +"'";
+    
+    $odb->insertquery($sqlstr);
+    
+    echo "The file has been uploaded.";
 } else {
 	echo "Sorry, there was an error uploading your file.";
 }
-*/
