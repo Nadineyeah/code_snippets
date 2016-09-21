36fe560c025f0ceee923bb03323c1aba494fb12b
fcipriano <etefcipriano@gmail.com>
2016-09-21 17:11:29 +0800

    Create index.php

---
+++ jquery-ajax-JSON/index.php
@@ -0,0 +1,13 @@
+<script src="jquery.js"></script>
+<script src="file.js"></script>
+
+<div>
+	Table of: <input type="text" id="num" /><br/>
+	<button id="btngenerate">Generate Table</button>
+	<br />
+	<div id="divtbl">
+		<table border='1' id='tblmultiply'>
+			
+		</table>
+	</div>
+</div>
