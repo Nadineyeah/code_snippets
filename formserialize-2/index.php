45f2999d88f0361e48ad3a84f1a1c91ec020513b
fcipriano <etefcipriano@gmail.com>
2016-09-28 12:03:31 +0800

    Create index.php

---
+++ formserialize-2/index.php
@@ -0,0 +1,19 @@
+<script src='jquery.js'></script>
+<script src='filejs.js'></script>
+
+<form id='frmuser'>
+	<h3>Student Name</h3>
+	First Name:&nbsp;<input type='text' name='fname'/><br />
+	Last Name:&nbsp;<input type='text' name='lname'/><br />
+	Subject:&nbsp;<input type='text' name='subj'/><br /><br />
+	
+	<h3>Instructor Name</h3>
+	First Name:&nbsp;<input type='text' name='ifname'/><br />
+	Last Name:&nbsp;<input type='text' name='ilname'/><br />
+	
+	
+	<button id='btnsubmit'>Submit</button>
+</form>
+
+<div id='showname'>
+<div>
