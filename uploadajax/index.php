764fc5412cdfc778941cee55e763e5fa8fdf730b
fcipriano <etefcipriano@gmail.com>
2016-10-03 11:49:12 +0800

    Create index.php

---
+++ uploadajax/index.php
@@ -0,0 +1,20 @@
+<?php include('header.php'); ?>
+
+<div class="container">
+	
+	<h1>Home Page</h1>
+	<div class="row">
+		<div class="col-md-6 col-lg-6">
+			<input type="file" class="form-control" name="profilepic" id="profilepic"/><br />
+			<button id="btnsubmit" class="btn btn-default" >Submit</button>
+		</div>
+		<div class="col-md-6 col-lg-6">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8 col-lg-8">
+			<img id="imgprev" alt="image" class="thumbnail"/>
+		</div>
+	</div>
+</div>
+<?php include('footer.php'); ?>
